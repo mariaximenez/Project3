0feca51d805d1a2c07757aeb533e9b9ce28f9c40
Maria Jimenez <mtjimenez@gmail.com>
2022-05-16 14:14:21 -0400

    Routes add

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import Home from './Home'
 import BoroughList from './BoroughList'
 import CategoryList from './CategoryList'
 import Show from './Show'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
+    const URL = "https://data.cityofnewyork.us/resource/pqg4-dm6b.json/";
   return (
-    <div>
-        <Home/>
-        <BoroughList/>
-        <CategoryList/>
-        <Show/>
-    
-    
-    
-    </div>
+     <div className="App">
+     <Header/>
+     <Routes>
+         <Route exact path="/" element={<Home URL={URL}/>}/>
+         <Route path="/borough" element={<BoroughList URL={URL}/>}/>
+         <Route path="/category"  element= {<CategoryList URL={URL}/>}/>
+         <Route path="/:id"  element= {<Show URL={URL}/>}/>
+     </Routes>
+     <Footer/>
+   </div>
   )
 }
 
