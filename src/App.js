bf5e17491659e37313d0a5f155d911675dab5f4a
Maria Jimenez <mtjimenez@gmail.com>
2022-05-16 13:53:55 -0400

    Pages and Component display on Servers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Home from './Home'
+import BoroughList from './BoroughList'
+import CategoryList from './CategoryList'
+import Show from './Show'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <Home/>
+        <BoroughList/>
+        <CategoryList/>
+        <Show/>
+    
+    
+    
+    </div>
   )
 }
 
---
+++ src/BoroughList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BoroughList = () => {
+  return (
+    <div>BoroughList</div>
+  )
+}
+
+export default BoroughList
