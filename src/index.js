14a091d62c91e058fe4961b57dc48a1ba6ae36a4
Maria Jimenez <mtjimenez@gmail.com>
2022-05-16 13:41:21 -0400

    File Structure and basic page setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from 'react-dom';
+import "./index.css";
 import App from './App'
+import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById("root")
+  );
+  
