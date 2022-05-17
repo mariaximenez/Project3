ea33f0a395eec0261d654f692cc1533d69775cc6
Maria Jimenez <mtjimenez@gmail.com>
2022-05-17 13:48:03 -0400

    Search bar functioning

--- frontend2/src/components/SearchBar.js
+++ frontend2/src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import '../index.css';
-
+import SearchIcon from "@material-ui/icons/Search";
+import CloseIcon from "@material-ui/icons/Close";
 
 function SearchBar({placeholder, data}) {
   const [filteredData, setFilteredData] = useState([]);
+  const [worEntered, setWordEntered] = useState('');
   const handleFilter = (event) => {
     const searchWord = event.target.value
     const newFilter = data.filter((value) => {
@@ -11,11 +13,32 @@ function SearchBar({placeholder, data}) {
     });
     setFilteredData(newFilter);
 };
+  if (searchWord === '') {
+    setFilteredData([]);
+  } else {
+    setFilteredData(newFilter);
+  }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
   return (
     <div className= "search">
         <div className= "searchInputs">
-            <input type="text" placeholder={placeholder} onChange={handleFilter}/>
-        <div className="searchIcon"></div>
+            <input type="text"
+            placeholder={placeholder}
+            value={wordEntered}
+            onChange={handleFilter}/>
+        <div className="searchIcon">
+          {filteredData.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearBtn" onClick={clearInput}/>
+        )}
+        </div>
         </div>
         {filteredData.length != 0 && (
         <div className= "dataResult">
@@ -28,13 +51,11 @@ function SearchBar({placeholder, data}) {
           );
         })}
             </div>
-,
+        )}
              </div>
   );
       }
            
          
-        
-  
 
-export default SearchBar
+export default SearchBar;
